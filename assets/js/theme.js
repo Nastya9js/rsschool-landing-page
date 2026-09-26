f476bbc85393df2357cbfb807b3ee4707f64a3a3
Nastya9js <anastasiya@zota.com>
2026-09-26 04:56:36 +0300

    feat: modify scheme based on the user preferences

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,15 +1,31 @@
-const themeSwitch = document.querySelector(".theme-switch");
-
 const savedTheme = localStorage.getItem("theme");
 
-if (savedTheme === "dark") {
-  document.body.classList.add("dark");
+if (savedTheme) {
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+} else {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+  }
 }
+
+const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+darkModeQuery.addEventListener("change", (event) => {
+  if (!localStorage.getItem("theme")) {
+    if (event.matches) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+});
+
+const themeSwitch = document.querySelector(".theme-switch");
 
 themeSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
   const currentTheme = document.body.classList.contains("dark") ? "dark" : "light";
-
   localStorage.setItem("theme", currentTheme);
 });
